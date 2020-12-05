95d5986103261d996ee45cd51032ed9343a498e9
ry brown <ryan.alex.brown@gmail.com>
2020-12-04 23:45:57 -0700

    time and comments

--- controllers/pizza-controllers.js
+++ controllers/pizza-controllers.js
@@ -3,6 +3,12 @@ const { Pizza } = require('../models');
 const pizzaController = {
     getAllPizza(req, res) {
         Pizza.find({})
+        .populate({
+            path: 'comments',
+            select: '-__V'
+        })
+        .select('-__v')
+        .sort({_id: -1})
           .then(dbPizzaData => res.json(dbPizzaData))
           .catch(err => {
             console.log(err);
@@ -13,6 +19,11 @@ const pizzaController = {
       // get one pizza by id
       getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
+        .populate({
+            path: 'comments',
+            select: '-__v'
+          })
+          .select('-__v')
           .then(dbPizzaData => {
             // If no pizza is found, send 404
             if (!dbPizzaData) {
